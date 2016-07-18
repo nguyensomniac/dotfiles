341e0fe181f969d6ed4769ba1a44324c04e90951
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2016-07-18 03:47:51 -0400

    add tmux.conf and more installed packages

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap caskroom/cask
 brew install brew-cask
 brew tap caskroom/versions
+brew tap caskroom/fonts
 
 # Install brew packages
 
@@ -25,11 +26,26 @@ brew install "${brew_apps[@]}"
 
 cask_apps=(
   atom
+  dashlane
+  firefox
+  flux
   google-chrome
+  iterm2-beta
+  polymail
+  sketch
+  slack
   spotify
 )
 
-brew-cask install "${cask_apps[@]}"
+brew cask install "${cask_apps[@]}"
+
+cask_fonts=(
+  font-input
+  font-karla
+  font-source-sans-pro
+)
+
+brew cask install "${cask_fonts[@]}"
 
 npm_packages=(
   babel
@@ -38,7 +54,42 @@ npm_packages=(
   gulp
   react
   redux
+  webpack
   yo
 )
 
 npm install -g "${npm_packages[@]}"
+
+pip_packages=(
+  matplotlib
+  pandas
+)
+
+pip3 install "${pip_packages[@]}"
+
+atom_packages=(
+  aesthetic-ui
+  base16-syntax
+  file-icons
+  firewatch-syntax
+  pigments
+  power-mode
+  project-manager
+  redmond-syntax
+  spacegray-dark-syntax
+  spacegray-dark-ui
+  spacegray-light-syntax
+  spacegray-light-ui
+  vim-mode
+)
+
+apm install "${atom_packages[@]}"
+
+# Install oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Change default shell to zsh
+sudo -s
+echo $(which zsh) >> /etc/shells
+exit
+chsh -s $(which zsh)
